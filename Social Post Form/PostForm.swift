1b86fbeab93694fd8a2b7eea25a97ff3d094af97
yokobuto <info@yokobuto.de>
2025-04-27 20:20:06 +0200

    Sheet added

---
+++ Social Post Form/PostForm.swift
@@ -0,0 +1,18 @@
+//
+//  PostForm.swift
+//  Social Post Form
+//
+//  Created by Jörg Klausewitz on 27.04.25.
+//
+
+import SwiftUI
+
+struct PostForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostForm()
+}
